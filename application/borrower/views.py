2f35762a77db972798c83cc3e240677f4db05a2e
Dan Marker <daniel.marker@digital.landregistry.gov.uk>
2015-11-20 15:33:24 +0000

    Correction post linting check

--- application/borrower/views.py
+++ application/borrower/views.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, render_template
 
 borrower_landing = Blueprint('borrower_landing', __name__,
-                   template_folder='/templates',
-                   static_folder='static')
+                             template_folder='/templates',
+                             static_folder='static')
 
 
 @borrower_landing.route('/')
