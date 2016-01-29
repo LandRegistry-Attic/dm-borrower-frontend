f725c445d6ca79367013d4720f14321f35388db7
Victor Akinmade <victor.akinmade@digital.landregistry.gov.uk>
2016-01-29 14:30:10 +0000

    adjusted indentation for deed/searchdeed/views.py

--- application/deed/searchdeed/views.py
+++ application/deed/searchdeed/views.py
@@ -13,6 +13,7 @@ searchdeed = Blueprint('searchdeed', __name__,
 def search_deed_main():
     return render_template('searchdeed.html', error=None)
 
+
 @searchdeed.route('/finished', methods=['POST'])
 def show_final_page():
     return render_template('finished.html')
