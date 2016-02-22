9ba4ba84dcf1735223275e29259402b3bd76a3f3
Abbas Ghulam <g.abbas@kainos.com>
2016-02-22 16:25:46 +0000

    Fixed failing unit tests

--- tests/test_search_deed.py
+++ tests/test_search_deed.py
@@ -2,7 +2,8 @@ from tests.helpers import with_client, setUpApp, with_context
 import unittest
 from application.deed.searchdeed.views import validate_dob
 from datetime import date
-
+import mock
+from flask.ext.api import status
 
 class TestSearchDeed(unittest.TestCase):
     def setUp(self):
