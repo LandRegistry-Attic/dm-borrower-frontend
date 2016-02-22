7c0f2c116fdc7545d810d8077680cab0ff005695
Abbas Ghulam <g.abbas@kainos.com>
2016-02-22 09:03:02 +0000

    Basic implementation for signing a deed - WIP

--- application/deed/searchdeed/views.py
+++ application/deed/searchdeed/views.py
@@ -55,6 +55,7 @@ def enter_dob():
             deed_token = validate_borrower(form['borrower_token'], dob)
             if deed_token is not None:
                 session['deed_token'] = deed_token['deed_token']
+                session['borrower_token'] = form['borrower_token']
                 return redirect('/how-to-proceed', code=307)
             else:
                 session['error'] = "True"
@@ -110,3 +111,21 @@ def lookup_deed(deed_reference):
         deed_data = None
 
     return deed_data
+
+
+def sign_deed_with(deed_reference, and_borrower_id):
+    deed_api_client = getattr(searchdeed, 'deed_api_client')
+    response = deed_api_client.add_borrower_signature(deed_reference, and_borrower_id)
+    return response
+
+
+# This should be replaced with an api call to deed_api for borrower_id - WIP (Need to discuss)
+def get_borrower_id():
+    deed_data = lookup_deed(session['deed_token'])
+    borrower_id = None
+    if deed_data is not None:
+        for borrower in deed_data['deed']['borrowers']:
+            if borrower['token'] == session.get('borrower_token'):
+                borrower_id = {"borrower_id": borrower['id']}
+
+    return borrower_id
