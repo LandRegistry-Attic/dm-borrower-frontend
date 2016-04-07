e43c48776e1476390ada615cb45534369cedeaf4
Victor Akinmade <victor.akinmade@digital.landregistry.gov.uk>
2016-04-07 15:18:00 +0100

    refactored app key environment to use environment variable

--- application/__init__.py
+++ application/__init__.py
@@ -5,6 +5,7 @@ from .health.views import health
 from .deed.searchdeed.views import searchdeed
 from .borrower.views import borrower_landing
 from datetime import timedelta
+from application import config
 import os
 import logging
 from logger import logging_config
@@ -28,7 +29,7 @@ def create_manager(deed_api_client=make_deed_api_client()):
     app.register_blueprint(health, url_prefix='/health')
     app.register_blueprint(searchdeed)
     app.register_blueprint(borrower_landing)
-    app.secret_key = os.urandom(32)
+    app.secret_key = config.APP_SECRET_KEY
 
     app.permanent_session_lifetime = timedelta(minutes=20)
 
