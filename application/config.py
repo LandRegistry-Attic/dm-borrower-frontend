11542e3c91ffb4b6ad7e9e5e3c60b4beaf209b60
Sophie Drake <s.drake@kainos.com>
2016-04-12 12:04:58 +0100

    Check for verify config in environment variables

--- application/config.py
+++ application/config.py
@@ -2,7 +2,8 @@ import os
 
 DEBUG = True
 
-VERIFY = True
+VERIFY = os.getenv('VERIFY',
+                   False)
 
 DEED_API_BASE_HOST = os.getenv('DEED_API_ADDRESS',
                                'http://0.0.0.0:9020')
